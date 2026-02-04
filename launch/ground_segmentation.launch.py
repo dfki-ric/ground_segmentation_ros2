38f99fae8c1da7d048eb8f1c2ec761a659ddcadd
haider8645 <haider_lodhi@hotmail.com>
2026-02-04 06:09:08 +0000

    Add sim argument to launch

--- launch/ground_segmentation.launch.py
+++ launch/ground_segmentation.launch.py
@@ -4,41 +4,60 @@ from launch.substitutions import PathJoinSubstitution, LaunchConfiguration
 from launch_ros.substitutions import FindPackageShare
 from launch_ros.actions import Node
 
+
 def launch_setup(context, *args, **kwargs):
 
-    parameters = PathJoinSubstitution(
+    parameters_file = PathJoinSubstitution(
         [FindPackageShare("ground_segmentation_ros2"), "config", "parameters.yaml"]
     )
 
     ground_segmentation_ros2_node = Node(
         package="ground_segmentation_ros2",
         executable="ground_segmentation_ros2_node",
-        parameters=[parameters],
-        #arguments=['--ros-args', '--log-level', 'debug'],
-        remappings=[('/ground_segmentation/input_pointcloud', LaunchConfiguration('pointcloud_topic')),
-                    ('/ground_segmentation/input_imu', LaunchConfiguration('imu_topic'))],
+        parameters=[
+            parameters_file,
+            {
+                "use_sim_time": LaunchConfiguration("sim")
+            }
+        ],
+        remappings=[
+            ("/ground_segmentation/input_pointcloud", LaunchConfiguration("pointcloud_topic")),
+            ("/ground_segmentation/input_imu", LaunchConfiguration("imu_topic")),
+        ],
         output="screen",
     )
 
-    nodes_to_start = [ground_segmentation_ros2_node]
-    
-    return nodes_to_start
+    return [ground_segmentation_ros2_node]
+
 
 def generate_launch_description():
-    
+
     declared_arguments = []
+
     declared_arguments.append(
         DeclareLaunchArgument(
-            'pointcloud_topic',
-            default_value='/ground_segmentation/input_pointcloud',
-            description='Topic name for the pointcloud'
+            "pointcloud_topic",
+            default_value="/ground_segmentation/input_pointcloud",
+            description="Topic name for the pointcloud",
         )
     )
+
     declared_arguments.append(
         DeclareLaunchArgument(
-            'imu_topic',
-            default_value='/ground_segmentation/input_imu',
-            description='Topic name for the imu'
+            "imu_topic",
+            default_value="/ground_segmentation/input_imu",
+            description="Topic name for the imu",
         )
     )
-    return LaunchDescription(declared_arguments + [OpaqueFunction(function=launch_setup)])
+
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "sim",
+            default_value="false",
+            description="Use simulation time (set true for Gazebo / bag playback)",
+        )
+    )
+
+    return LaunchDescription(
+        declared_arguments + [OpaqueFunction(function=launch_setup)]
+    )
